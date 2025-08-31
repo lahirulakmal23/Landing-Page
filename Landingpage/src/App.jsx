a3b1a2ab2e15fc80a958607f78a392fea96f2fae
lahiru lakmal <145210689+lahirulakmal23@users.noreply.github.com>
2025-08-31 16:10:27 +0530

    add logo image to footer

--- Landingpage/src/App.jsx
+++ Landingpage/src/App.jsx
@@ -1,22 +1,34 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Header from './Components/Header'
 import Eventcart from './Components/Eventcart'
 import Service from './Components/Service'
 import Banner from './Components/Banner'
 import Footer from './Components/Footer'
+import About from "./Components/About";
 
+
+// Dummy pages for now (you can replace with real components later)
+
+
+function ServicePage() {
+  return <Service />;
+}
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Eventcart/>
-      <Service/>
-      <Banner/>
-      <Footer/>
-    </div>
+    <Router>
+      <Header /> 
+      <Routes>
+        <Route path="/Header" element={<Header />} />
+        <Route path="/Eventcart" element={<Eventcart />} />
+        <Route path="/Service" element={<ServicePage />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+      <Banner />
+      <Footer />
+    </Router>
   )
 }
 
-export default App
+export default App;
